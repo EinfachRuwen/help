6961ea7bfc082ce275b9b59777009c5d3f674b7b
byZeroOfficial <76685126+byZeroOfficial@users.noreply.github.com>
2021-09-08 19:42:20 +0200

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/byZeroOfficial/help',
+  docsRepositoryBase: 'hhttps://github.com/byZeroOfficial/help/blob/master',
+  titleSuffix: ' – help.byzero.dev',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">help.byzero.dev</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Get help
       </span>
     </>
   ),
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Get help" />
+      <meta name="og:description" content="Get help" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://byzero.dev/image/me.gif" />
+      <meta name="twitter:site:domain" content="help.byzero.dev" />
+      <meta name="twitter:url" content="https://help.byzero.dev" />
+      <meta name="og:title" content="help.byzero.dev - Created by byZero" />
+      <meta name="og:image" content="https://byzero.dev/image/me.gif" />
+      <meta name="apple-mobile-web-app-title" content="help.byzero.dev" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -60,8 +60,8 @@ export default {
   search: true,
   prevLinks: true,
   nextLinks: true,
-  footer: true,
+  footer: false,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © byZero.</>,
   unstable_faviconGlyph: '👋',
 }
