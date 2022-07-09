7ba1b5ee017e7e9d3685eaceb8ee825e0ddef160
byZeroOfficial <mail@byzero.dev>
2022-07-09 15:04:56 +0200

    s

--- theme.config.js
+++ theme.config.js
@@ -16,8 +16,8 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Get help" />
-      <meta name="og:description" content="Get help" />
+      <meta name="description" content="Tips and more" />
+      <meta name="og:description" content="Tips and more" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://byzero.dev/image/me.gif" />
       <meta name="twitter:site:domain" content="help.byzero.dev" />
